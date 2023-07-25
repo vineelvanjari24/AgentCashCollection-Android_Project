22d974ec55550189c664f726fe3b36f1d1408ecb
Shanmukh Kuppili <shanmukhkuppili@gmail.com>
2023-07-25 10:03:52 +0530

    UploadCollection.java and DownloadCustomer.java update

---
+++ app/src/main/java/com/example/agentcashcollection/admin/DownloadCustomer.java
@@ -0,0 +1,16 @@
+package com.example.agentcashcollection.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agentcashcollection.R;
+
+public class DownloadCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download_customer);
+    }
+}
--- app/src/main/java/com/example/agentcashcollection/admin/UploadDownload.java
+++ app/src/main/java/com/example/agentcashcollection/admin/UploadDownload.java
@@ -2,6 +2,8 @@ package com.example.agentcashcollection.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,22 +11,26 @@ import com.example.agentcashcollection.R;
 
 public class UploadDownload extends AppCompatActivity {
 
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_download);
 
+        context = this;
+
         findViewById(R.id.uploadCollection).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(context, UploadCollection.class));
             }
         });
 
         findViewById(R.id.downloadCustomer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(context, DownloadCustomer.class));
             }
         });
     }
